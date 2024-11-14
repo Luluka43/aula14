a6d468dd589daa9d3e749461ffef55c0590acadd
Luluka43 <setentrionalhs@gmail.com>
2024-11-14 09:54:08 -0300

    exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -16,3 +16,12 @@ st.write('Você selecionou: ', opcao)
 
 dadosFiltrados = df[df['nomeServidor'] == opcao]
 dadosFiltrados
+
+
+
+
+import streamlit as st
+import pandas as pd
+
+st.title('Localização das comunidades quilombolas (2022)')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
